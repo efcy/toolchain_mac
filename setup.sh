de2f63b52352b067fd7eb849c7a2cb1b1023b218
Stella Alice Schlotter <stella.a.schlotter@gmail.com>
2025-12-13 21:58:37 +0100

    fix: make the toolchain more minimal

--- setup.sh
+++ setup.sh
@@ -63,57 +63,6 @@ fi
 . ~/.profile
 
 echo "-----------------------------------"
-echo "- Generate projectconfig.user.lua -"
-echo "-----------------------------------"
-
-cat >projectconfig.user.lua <<EOL
--- special pathes which can be configured manualy.
--- If a path is set to 'nil' the default value is used.
--- for default values check projectconfig.lua
-
--- default: "../../Framework"
-FRAMEWORK_PATH = nil
-
--- for native platform
--- default: "../../Extern"
-EXTERN_PATH_NATIVE = "${EXTERN_PATH_NATIVE}"
-
--- path to the crosscompiler and libs
--- default: os.getenv("NAO_CTC")
-NAO_CTC = "${NAO_CTC}"
--- or set both explicitely
--- default: NAO_CTC .. "/compiler"
-COMPILER_PATH_NAO = nil
--- default: NAO_CTC .. "/extern"
-EXTERN_PATH_NAO = nil
-
--- naoqi toolchain needed to compile the NaoSMAL
--- default: os.getenv("AL_DIR")
-AL_DIR = nil
-
--- optional: manualy enable/disable colors
--- print_colors = true
-
-if PLATFORM == "Nao" then
-  _OPTIONS["crosscompiler"] = "clang" -- set "gcc" for legacy compilation
-end
-
-function set_user_defined_paths() 
-
-  -- add your additional include directories here
-  -- sysincludedirs { "my/nao/includes/path1" }
-
-  -- add your additional lib directories here
-  --syslibdirs { "my/nao/libs/path1" }
-
-  -- NOTE: this should be used only for project internal files
-  -- use this ONLY if you know what you are doing
-  --includedirs {}
-  --libdirs {}
-end
-EOL
-
-echo "-----------------------------------"
 echo "- compiling external dependencies -"
 echo "-----------------------------------"
 
